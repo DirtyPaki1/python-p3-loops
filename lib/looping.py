00704aa4ad2c8c0f593dfc7a0818bb02f987f6f5
Usman <91799557+DirtyPaki1@users.noreply.github.com>
2024-05-29 16:03:59 -0400

    Completed

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,28 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    for i in range(10, 0, -1):  # Countdown from 10 to 1
+        print(i)
+    print("Happy New Year!")  # Print "Happy New Year!" after the countdown
 
-def square_integers(int_list):
-    # code goes here!
-    pass
+
+def square_integers(numbers):
+    result = []
+    for number in numbers:
+        result.append(number ** 2)
+    return result
+
+
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    '''prints 1 to 100 with fizz 3s, buzz 5s, fizzbuzz 3and5s'''
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+
